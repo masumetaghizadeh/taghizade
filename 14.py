9ad2e4a821ba9f9127cbdb0d709a9668a0b3058a
masume taghizadeh <120125773+masumetaghizadeh@users.noreply.github.com>
2022-12-14 09:33:46 +0330

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,6 +1,7 @@
 import math
 print("welcome to my calculator")
 a=int(input("please enter a number: "))
+x=math.radians(a)
 op=input("please enter operation: ")
 if op=="sqrt":
     result=math.sqrt(a)
@@ -11,7 +12,7 @@ if op=="cos":
 if op=="tan":   
     result=math.tan(a)
 if op=="cot":  
-    result=math.cot(a)
+    result=1/(math.tan(x))
 if op=="factorial":  
     result=math.factorial(a)
 if op=="+":
