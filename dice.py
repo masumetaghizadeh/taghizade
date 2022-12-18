04e013f7ae65c72b2a9c54a1217de6822c761ff5
masume taghizadeh <120125773+masumetaghizadeh@users.noreply.github.com>
2022-12-18 13:20:20 +0330

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,14 +1,14 @@
 import random
 while True:
     user = input('Do you want to roll the dice??  (y/n) : ')
-    if user == 'Y':
+    if user == 'y':
         a = random.randint(1,6)
         print(a)
         if a==6:
             print("you won")
             for i in range(2):
                 user = input("You are allowed to roll the dice two more times (y/n):")
-                if user == "Y":
+                if user == "y":
                      a = random.randint(1,6)
                      print(a)
     else:
