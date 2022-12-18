9a867ec2e9719e54fff0c879597fd885ba1432b0
masume taghizadeh <120125773+masumetaghizadeh@users.noreply.github.com>
2022-12-18 14:23:57 +0330

    Update شمارش تعداد حدس عدد.py

--- شمارش تعداد حدس عدد.py
+++ شمارش تعداد حدس عدد.py
@@ -1,7 +1,7 @@
 import random
 from collections import Counter
 pc_number=random.randint(0,20)
-Counter=1
+Counter=0
 while True:
     user_number=int(input("enter number:"))
     if pc_number==user_number:
